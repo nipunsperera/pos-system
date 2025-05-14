e950c52bd078d8d35b9878d0116cd382e5b07b7b
nipunsperera <samadinipun@gmail.com>
2025-05-14 22:42:30 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',              // entry HTML
+    './src/**/*.{js,ts,jsx,tsx}' // all files in src with these extensions
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
